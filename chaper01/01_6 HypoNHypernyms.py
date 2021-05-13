a893eda6c233b2d3a954fb0c16527bc17fb5b87a
hyeryn <hyeryn2000@naver.com>
2021-05-13 19:12:07 +0900

    말뭉치와 워드넷

--- chaper01/01_6 HypoNHypernyms.py
+++ chaper01/01_6 HypoNHypernyms.py
@@ -3,7 +3,10 @@ woman = wn.synset('woman.n.01')
 bed = wn.synset('bed.n.01')
 
 print(woman.hypernyms()) #동일한 직계부모 노드의 동의어 집합 반환 -> woman : 성인&여성의 범주에 속함
-woman_paths = woman.hyperny_paths() #집합의 리스트를 반환
+woman_paths = woman.hypernym_paths() #집합의 리스트를 반환
 
 # 루트 노드에서 woman 노드로 경로를 출력
-for idx, path
+for idx, path in enumerate(woman_paths):
+    print('\n\n상위어 경로 : ', idx + 1)
+    for synset in path:
+        print(synset.name(), ', ', end='')
